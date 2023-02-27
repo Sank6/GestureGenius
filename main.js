a443c5778b7eef63b78f7c25b5e3e21561c7478a
Sank6 <25014241+Sank6@users.noreply.github.com>
2023-02-27 03:09:39 +0000

    Co-authored-by: amicazb <amicazb@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
+const gestureMap = {
+    0: 'A',
+    1: 'B',
+    2: '',
+    3: 'C',
+    4: 'D',
+    5: 'E',
+    6: 'F',
+    7: 'G',
+    8: 'H',
+    9: 'I',
+    10: 'J',
+    11: 'K',
+    12: 'L',
+    13: 'M',
+    14: 'N',
+    15: 'O',
+    16: 'P',
+    17: 'Q',
+    18: 'R',
+    19: 'S',
+    20: 'T',
+    21: 'U',
+    22: 'V',
+    23: 'W',
+    24: 'X',
+    25: 'Y',
+    26: 'Z'
+};
 
 const load = async () => {
     navigator.mediaDevices.getUserMedia({ video: true })
@@ -9,20 +38,43 @@ const load = async () => {
             console.log('Unable to access camera:', error);
         });
 
-    const model = await tf.loadLayersModel('/model.json');
+    const handposeModel = await handpose.load();
+    console.log('Handpose Model loaded');
+    const aslModel = await tf.loadLayersModel('/model/model.json');
+    console.log('ASL Model loaded');
     const webcam = await tf.data.webcam(document.getElementById('video'));
-    const prediction = document.getElementById('prediction');
-    
+    console.log('Webcam loaded');
+
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    let letter = letters[Math.floor(Math.random() * letters.length)];
+    document.getElementById('img').src = `/images/${letter}.png`;
+    console.log(`/images/${letter}.png`);
+
     while (true) {
         const img = await webcam.capture();
-        const processedImg = tf.tidy(() => {
-            return img.expandDims(0).toFloat().div(127).sub(1);
-        });
-        const result = await model.predict(processedImg).data();
-        const output = result[0] > result[1] ? '👍' : '👎';
-        prediction.innerText = `Prediction: ${output}`;
+        const predictions = await handposeModel.estimateHands(img);
+
+        if (predictions.length > 0) {
+            const landmarks = predictions[0].landmarks;
+            const flatLandmarks = [].concat(...landmarks);
+            const gesture = await predictGesture(aslModel, flatLandmarks);
+            document.getElementById('prediction').innerText = gesture;
+        }
         img.dispose();
+
+        
+        if (document.getElementById('prediction').innerText === letter.toUpperCase()) {
+            letter = letters[Math.floor(Math.random() * letters.length)];
+            document.getElementById('img').src = `/images/${letter}.png`;
+        }
+
         await tf.nextFrame();
     }
 }
 
+const predictGesture = async (model, hand) => {
+    const prediction = await model.predict(tf.tensor2d([hand])).data();
+    const max = prediction.indexOf(Math.max(...prediction));
+    const gesture = gestureMap[max];
+    return gesture;
+}
